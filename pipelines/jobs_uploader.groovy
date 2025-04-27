24f0b370d301a159abc5eb5e9dadf7b3e0e336a4
NickMalykhin <popneo2@gmail.com>
2025-04-27 10:18:51 +0000

    fix Owner7

--- pipelines/jobs_uploader.groovy
+++ pipelines/jobs_uploader.groovy
@@ -1,109 +1,55 @@
 
-// timeout(300) {
-//     node('python') {
-        
-
-//         currentBuild.description = """
-//         BRANCH=${REFSPEC}
-//         Owner=${env.BUILD_USER ?: 'Automated (SCM)'}
-//         """
-//                 // Owner=${env.BUILD_USER}
-
-//         stage('Checkout') {
-//             dir('api-tests') {
-//                 checkout scm
-//             }
-//         }
-
-//         // stage('Runnung test') {
-//         //     extCode = sh(
-//         //         script: 'sudo docker run --rm --name=uitests -t localhost:5005/ui_tests:1.0.0',
-//         //         returnStatus: true
-//         //     )
-//         // }
-
-
-//         // stage('Publish allure report') {
-//         //     dir('api-tests') {
-//         //         allure({
-//         //             includeProperties: false,
-//         //             jdk: '',
-//         //             properties: [],
-//         //             reportBuildPolicy: 'ALWAYS',
-//         //             results: [[path: './allure-results']]
-//         //         })
-//         //     }
-//         // }
-
-//         def configScriptPath = './config/config.py'
-
-//         stage('Generate job.ini config') {
-//             dir('api-tests') {
-//                 withCredentials([usernamePassword(credentialsId: 'jobs_builder_creds', usernameVariable: 'username', passwordVariable: 'password')]) {
-//                     sh "USER=${username} PASSWORD=${password} python3 ${configScriptPath}"
-//                 }
-                
-//             }
-
-//         }
-
-//         stage('Start update jobs') {
-//             dir('api-tests') {
-//                 sh "jenkins-jobs --conf ./config/job.ini update ./jobs/"
-//             }
-//         }
-
-//     }
-// }
-
-
 timeout(300) {
     node('python') {
-        // Явно получаем пользователя через Build User Vars или параметр
-        def owner = env.BUILD_USER ?: 'Automated (SCM)'
+        
 
         currentBuild.description = """
-        BRANCH=${env.GIT_BRANCH}
-        Owner=${owner}
-        """.stripIndent()
+        BRANCH=${REFSPEC}
+        Owner=${env.BUILD_USER}
+        """
+                // Owner=${env.BUILD_USER}
 
         stage('Checkout') {
             dir('api-tests') {
-                checkout scm  // Используем стандартный checkout
+                checkout scm
             }
         }
 
-        stage('Running tests') {
-            extCode = sh(
-                script: 'sudo docker run --rm --name=uitests -t localhost:5005/ui_tests:1.0.0',
-                returnStatus: true
-            )
+        stage('Debug') {
+            echo "Build causes: ${currentBuild.getBuildCauses()}"
+            echo "Build user: ${env.BUILD_USER}"
         }
 
-        stage('Publish Allure Report') {
-            dir('api-tests') {
-                allure([
-                    includeProperties: false,
-                    jdk: '',
-                    properties: [],
-                    reportBuildPolicy: 'ALWAYS',
-                    results: [[path: './allure-results']]
-                ])
-            }
-        }
+        // stage('Runnung test') {
+        //     extCode = sh(
+        //         script: 'sudo docker run --rm --name=uitests -t localhost:5005/ui_tests:1.0.0',
+        //         returnStatus: true
+        //     )
+        // }
+
+
+        // stage('Publish allure report') {
+        //     dir('api-tests') {
+        //         allure({
+        //             includeProperties: false,
+        //             jdk: '',
+        //             properties: [],
+        //             reportBuildPolicy: 'ALWAYS',
+        //             results: [[path: './allure-results']]
+        //         })
+        //     }
+        // }
 
         def configScriptPath = './config/config.py'
 
         stage('Generate job.ini config') {
             dir('api-tests') {
-                withCredentials([usernamePassword(
-                    credentialsId: 'jobs_builder_creds',
-                    usernameVariable: 'USERNAME',
-                    passwordVariable: 'PASSWORD'
-                )]) {
-                    sh "USER=${USERNAME} PASSWORD=${PASSWORD} python3 ${configScriptPath}"
+                withCredentials([usernamePassword(credentialsId: 'jobs_builder_creds', usernameVariable: 'username', passwordVariable: 'password')]) {
+                    sh "USER=${username} PASSWORD=${password} python3 ${configScriptPath}"
                 }
+                
             }
+
         }
 
         stage('Start update jobs') {
@@ -111,5 +57,6 @@ timeout(300) {
                 sh "jenkins-jobs --conf ./config/job.ini update ./jobs/"
             }
         }
+
     }
 }
